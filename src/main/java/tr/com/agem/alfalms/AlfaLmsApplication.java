599d49695c345ffc4042e6d5a93e180d90058c2c
Emre Akkaya <emre.akkaya@agem.com.tr>
2018-07-24 10:38:45 +0300

    LMS initial commit

---
+++ src/main/java/tr/com/agem/alfalms/AlfaLmsApplication.java
@@ -0,0 +1,12 @@
+package tr.com.agem.alfalms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaLmsApplication.class, args);
+	}
+}
---
+++ src/main/java/tr/com/agem/alfalms/controller/ApiController.java
@@ -0,0 +1,20 @@
+package tr.com.agem.alfalms.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	
+	private static final Logger log = LoggerFactory.getLogger(ApiController.class);
+	
+	// TODO https://agem-bilisim.github.io/alfa-server/lms-integration.html
+	
+	// TODO 1: apiusers
+	
+	// TODO 2: apieducations
+	
+	// TODO 3: apieducationreport
+
+}
